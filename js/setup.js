77000bffeefb45423534b52cae30bdeac9d53315
Truneva Nataliya <truneva.nataliya@mail.ru>
2020-06-14 17:14:55 +0300

    Создали похожих персонажей

---
+++ js/setup.js
@@ -0,0 +1,47 @@
+'use strict';
+var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+var WIZARD_COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+var WIZARD_EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
+
+var userDialog = document.querySelector('.setup');
+userDialog.classList.remove('hidden');
+
+var similarListElement = userDialog.querySelector('.setup-similar-list');
+
+var similarWizardTemplate = document.querySelector('#similar-wizard-template')
+  .content
+  .querySelector('.setup-similar-item');
+
+var wizards = [];
+var createWizards = function () {
+  for (var i = 0; i < 4; i++) {
+    var wizard = {
+      name: WIZARD_NAMES[Math.round(Math.random() * 7)] + ' ' + WIZARD_SURNAMES[Math.round(Math.random() * 7)],
+      coatColor: WIZARD_COAT_COLORS[Math.round(Math.random() * 5)],
+      eyesColor: WIZARD_EYES_COLORS[Math.round(Math.random() * 4)]
+    };
+    wizards.push(wizard);
+  }
+  return wizards;
+};
+createWizards();
+
+var renderWizard = function (wizard) {
+  var wizardElement = similarWizardTemplate.cloneNode(true);
+
+  wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
+  wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
+  wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
+
+  return wizardElement;
+};
+
+var fragment = document.createDocumentFragment();
+for (var i = 0; i < wizards.length; i++) {
+  fragment.appendChild(renderWizard(wizards[i]));
+}
+similarListElement.appendChild(fragment);
+
+userDialog.querySelector('.setup-similar').classList.remove('hidden');
+
